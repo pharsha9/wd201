0d42e84fba482d3f76a8a704cb87c5cd93734a21
pharsha9 <116417608+pharsha9@users.noreply.github.com>
2023-12-19 17:09:46 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -3,16 +3,16 @@ const todos = todoList();
 
 describe("TodoList Test Suite", () => {
     beforeAll(() => {
-        const testTodo = {
+        todos.add({
             title: "Test Todo",
             completed: false,
             dueDate: new Date().toLocaleDateString("en-CA"),
-        };
-        todos.add(testTodo);
+        });
     });
 
     test("Should add a new Todo", () => {
         const initialTodoCount = todos.all.length;
+
         todos.add({
             title: "Test Todo",
             completed: false,
@@ -23,17 +23,28 @@ describe("TodoList Test Suite", () => {
     });
 
     test("Mark a todo as complete", () => {
+        const initialCompletionStatus = todos.all[0].completed;
+
+        todos.markAsComplete(0);
+
+        expect(todos.all[0].completed).toBe(!initialCompletionStatus);
+    });
+
+    test("List of overdue todos", () => {
+        const initialOverdueCount = todos.overdue().length;
+
         todos.add({
             title: "Overdue Todo",
             completed: false,
             dueDate: new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleDateString("en-CA"),
         });
 
-        expect(todos.overdue().length).toBe(todos.all.length + 1);
+        expect(todos.overdue().length).toBe(initialOverdueCount + 1);
     });
 
     test("List of todos due today", () => {
         const initialDueTodayCount = todos.dueToday().length;
+
         todos.add({
             title: "dueToday Todo",
             completed: false,
@@ -45,10 +56,11 @@ describe("TodoList Test Suite", () => {
 
     test("List of todos due later", () => {
         const initialDueLaterCount = todos.dueLater().length;
+
         todos.add({
             title: "UnderDue Todo",
             completed: false,
-            dueDate: new Date().toLocaleDateString("en-CA"),
+            dueDate: new Date(new Date().setDate(new Date().getDate() + 1)).toLocaleDateString("en-CA"),
         });
 
         expect(todos.dueLater().length).toBe(initialDueLaterCount + 1);
